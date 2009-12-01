dc518b855c0767f7a3c6fe895d33e03374d5542a
Mike Boers <mikeboers@maxwell.local>
2009-12-01 16:09:40 -0500

    wip

--- _crypto.pyx
+++ _crypto.pyx
@@ -4,6 +4,8 @@ cdef extern from "tomcrypt.h":
     ctypedef struct c_symmetric_key "symmetric_key":
         pass
     int c_aes_setup "aes_setup" (unsigned char *key, int keylen, int rounds, c_symmetric_key *skey)
+    int c_aes_ecb_encrypt "aes_ecb_encrypt" (char *pt, char *ct, c_symmetric_key *skey)
+    int c_aes_ecb_decrypt "aes_ecb_encrypt" (char *ct, char *pt, c_symmetric_key *skey)
     char * c_error_to_string "error_to_string" (int err)
 
 CRYPT_OK = c_CRYPT_OK
@@ -29,6 +31,17 @@ cdef class AES(object):
         ret = c_aes_setup(self.key, keylen, 0, &self.skey)
         if ret != CRYPT_OK:
             raise CryptoError(ret)
+    
+    def encrypt(self, plaintext):
+        cdef char ciphertext[16]
+        c_aes_ecb_encrypt(plaintext, ciphertext, &self.skey)
+        return ciphertext
+    
+    def decrypt(self, ciphertext):
+        cdef char plaintext[16]
+        c_aes_ecb_encrypt(ciphertext, plaintext, &self.skey)
+        return plaintext      
+        
     
             
     

f1e8728d9e011745cb197d69215dc6e47685666e
Mike Boers <mikeboers@maxwell.local>
2009-12-01 16:09:40 -0500

    wip

--- crypto.py
+++ crypto.py
@@ -1,8 +1,12 @@
 
+from base64 import b64encode
+
 from _crypto import AES
 
 if __name__ == '__main__':
-    
-    print 'hello'
-    
+        
     aes = AES('0123456789abcdef')
+    enc = aes.encrypt('hellothere1234567890')
+    dec = aes.decrypt(enc)
+    
+    print dec, b64encode(enc)

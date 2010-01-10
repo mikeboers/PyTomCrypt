338292026be093f58c4eddc06db5af6407114742
Mike Boers <github@mikeboers.com>
2010-01-10 12:22:35 -0500

    Wrote test function with test vectors.
    It does not pass yet.

--- cipher.py
+++ cipher.py
@@ -1,5 +1,6 @@
 
 from base64 import b64encode
+import os
 
 from _cipher import *
 
@@ -12,6 +13,35 @@ class Cipher(_Cipher):
 			self.decrypt = getattr(self, mode + '_decrypt')
 		except:
 			raise ValueError('no mode %r' % mode)
+
+def test():
+	for mode in 'ecb', 'cbc':
+		for type in 'encrypt', 'decrypt':
+			filename = './test_vectors/%s_%s_m.txt' % (mode, type[0])
+			fh = open(filename, 'rb')
+			lines = fh.readlines()[15:]
+			data = {}
+			for line in lines:
+				line = line.strip()
+				if not line and data:
+					print mode, type, data['i']
+					for k in 'iv', 'pt', 'ct':
+						if k in data:
+							print '\t%s: %s' % (k, data.get(k))
+					key = data['key'].decode('hex')
+					iv  = data.get('iv', '').decode('hex')
+					pt  = data['pt'].decode('hex')
+					ct  = data['ct'].decode('hex')
+					cipher = Cipher(key=key, iv=iv, cipher='aes', mode=mode)
+					if type == 'encrypt':
+						res = cipher.encrypt(pt)
+						assert res == ct, '%s != %s' % (res.encode('hex'), data['ct'])
+					data = {}
+				if line.count('=') != 1:
+					continue
+				k, v = line.lower().split('=')
+				data[k] = v
+				
 
 if __name__ == '__main__':
 		
@@ -26,3 +56,6 @@ if __name__ == '__main__':
 		cipher = Cipher(key, cipher='aes')
 		dec = getattr(cipher, mode + '_decrypt')(enc)
 		print mode, dec, enc.encode('hex'), iv.encode('hex')
+	
+	print
+	test()

fa5826c61d449c84de78f4f809018058b8f53bb0
Mike Boers <github@mikeboers.com>
2010-01-10 11:24:58 -0500

    All of the modes are supported.

--- cipher.py
+++ cipher.py
@@ -3,10 +3,22 @@ from base64 import b64encode
 
 from _cipher import *
 
+_Cipher = Cipher
+class Cipher(_Cipher):
+	def __init__(self, key, cipher='aes', mode='cbc'):
+		_Cipher.__init__(self, key, cipher, mode)
+		try:
+			self.encrypt = getattr(self, mode + '_encrypt')
+			self.decrypt = getattr(self, mode + '_decrypt')
+		except:
+			raise ValueError('no mode %r' % mode)
+
 if __name__ == '__main__':
-        
-    aes = Cipher('0123456789abcdef')
-    enc = aes.ecb_encrypt('0123456789abcdef')
-    dec = aes.ecb_decrypt(enc)
-    
-    print dec, b64encode(enc)
+		
+	key = '0123456789abcdef'
+	for mode in modes:
+		cipher = Cipher(key, cipher='aes')
+		enc = getattr(cipher, mode + '_encrypt')('0123456789abcdef')
+		cipher = Cipher(key, cipher='aes')
+		dec = getattr(cipher, mode + '_decrypt')(enc)
+		print mode, dec, b64encode(enc)

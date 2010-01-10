71c8fb92d28e587bded8d56f35e867e3aa1ed1d8
Mike Boers <github@mikeboers.com>
2010-01-10 14:25:01 -0500

    Only one symmetric_* per Cipher object.

--- cipher.py
+++ cipher.py
@@ -11,12 +11,12 @@ if __name__ == '__main__':
 		
 	key = '0123456789abcdef'
 	for mode in modes:
-		cipher = Cipher(key, cipher='aes')
-		enc = getattr(cipher, mode + '_encrypt')(key)
+		cipher = Cipher(key, cipher='aes', mode=mode)
+		enc = cipher.encrypt(key)
 		try:
 			iv = getattr(cipher, mode + '_get_iv')()
 		except AttributeError:
 			iv = ''
-		cipher = Cipher(key, cipher='aes')
-		dec = getattr(cipher, mode + '_decrypt')(enc)
+		cipher = Cipher(key, cipher='aes', mode=mode)
+		dec = cipher.decrypt(enc)
 		print mode, dec, enc.encode('hex'), iv.encode('hex')

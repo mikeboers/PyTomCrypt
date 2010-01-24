db28fe6f30b802e25a56cec000b26251572459b1
Mike Boers <github@mikeboers.com>
2010-01-24 10:59:33 -0500

    Fully remove hmac module.

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+ext_names = ['cipher', 'hash']
+
 ext_modules = [Extension(
     'tomcrypt.%s' % name,
     ["tomcrypt/%s.pyx" % name],
     include_dirs=['./libtomcrypt-1.16/src/headers'],
     extra_objects=['./libtomcrypt-1.16/libtomcrypt.a'],
-) for name in 'cipher', 'hash', 'hmac']
+) for name in ext_names]
 
 setup(
   name = 'PyTomCrypt',

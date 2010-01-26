43b58475cda7f99d2213ea90115062517d544c23
Mike Boers <github@mikeboers.com>
2010-01-26 11:09:28 -0500

    Slightly better hmac test.

--- tests/test_mac.py
+++ tests/test_mac.py
@@ -10,24 +10,40 @@ from subprocess import Popen, PIPE
 import hmac
 
 from tomcrypt import mac
+from tomcrypt._main import Error as TomCryptError
 
-
-def test_py_hmac():
-	for i in range(1, 10):
+def test_py_hmac_oneshot():
+	for i in range(1, 100):
 		key = os.urandom(8 * i)
 		x = hmac.new(key)
 		y = mac.hmac(key, hash='md5')
 		for j in range(1, 10):
 			v = os.urandom(j * 8)
-			print i, j
 			x.update(v)
 			y.update(v)
-			assert x.hexdigest() == y.hexdigest()
-	
+		assert x.hexdigest() == y.hexdigest()
+
+def test_py_hmac_multi():
+	i = j = -1
+	try:
+		for i in range(1, 100):
+			j = -1
+			key = os.urandom(8 * i)
+			x = hmac.new(key)
+			y = mac.hmac(key, hash='md5')
+			for j in range(1, 10):
+				v = os.urandom(j * 8)
+				x.update(v)
+				y.update(v)
+				assert x.hexdigest() == y.hexdigest()
+	except (AssertionError, TomCryptError):
+		assert False, 'failed on %d-%d' % (i, j)
+					
 if __name__ == '__main__':
 	start_time = time.time()
 	print 'Running internal tests...'
 	mac.test()
 	print 'Running against hmac module...'
-	test_py_hmac()
+	test_py_hmac_oneshot()
+	test_py_hmac_multi()
 	print 'Ran all tests in %.2fms' % (1000 * (time.time() - start_time))

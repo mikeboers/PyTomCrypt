aabeb35b62da7f8b34e185fcbaad6d33533174e8
Mike Boers <github@mikeboers.com>
2011-12-02 14:01:51 -0800

    WIP: Cleaning up pkcs5.

--- src/pkcs5.pyx
+++ src/pkcs5.pyx
@@ -1,29 +1,26 @@
-
 from tomcrypt._core cimport *
-from tomcrypt.hash cimport Descriptor as HashDescriptor
+from tomcrypt.hash cimport Descriptor as HashDescriptor, conform_hash
 from tomcrypt import Error
 
-cpdef bytes pkcs5_alg1(str password, str salt, int iteration_count, hash):
+
+cpdef bytes pkcs5_alg1(bytes password, bytes salt, int iteration_count, hash):
     if len(salt) != 8:
         raise Error('salt must be length 8')
-    cdef HashDescriptor desc = HashDescriptor(hash)
+    cdef HashDescriptor desc = conform_hash(hash)
     cdef unsigned long outlen = desc.digest_size
     out = PyBytes_FromStringAndSize(NULL, outlen)
     c_pkcs5_alg1(password, len(password), salt, iteration_count, desc.idx, out, &outlen)
     return out[:outlen]
 
 
-cpdef bytes pkcs5_alg2(str password, str salt, int iteration_count, hash, unsigned long outlen):  
-    cdef HashDescriptor desc = HashDescriptor(hash)
-    if not outlen:
-        outlen = desc.digest_size
+cpdef bytes pkcs5_alg2(bytes password, bytes salt, int iteration_count, hash):  
+    cdef HashDescriptor desc = conform_hash(hash)
+    cdef unsigned long outlen = desc.digest_size
     out = PyBytes_FromStringAndSize(NULL, outlen)
     c_pkcs5_alg2(password, len(password), salt, len(salt), iteration_count, desc.idx, out, &outlen)
     return out[:outlen]
 
 
-cpdef bytes pkcs5(str password, str salt='', int iteration_count=1024, hash='sha256', unsigned long length=0):
-    return pkcs5_alg2(password, salt, iteration_count, hash, length)
+pkcs5 = pkcs5_alg2
 
 
-__pkcs5_all__ = ('pkcs5_alg1', 'pkcs5_alg2', 'pkcs5')

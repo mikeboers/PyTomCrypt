e0ee3d3e6c15420ba16daa24a8e47c348c2c194f
Mike Boers <github@mikeboers.com>
2012-09-21 22:25:50 -0700

    Use assertEquals instead of assert

--- tests/test_prng.py
+++ tests/test_prng.py
@@ -21,23 +21,31 @@ class TestPRNG(TestCase):
         
         x = prng.fortuna()
         x.add_entropy(b'12345678')
-        # print x.read(16).encode('hex')
-        assert b16encode(x.read(16)).lower() == b'b1f2630e4b56ff6f1e0e5d6f1324a10d'
+        self.assertEqual(
+            b16encode(x.read(16)).lower(),
+            b'b1f2630e4b56ff6f1e0e5d6f1324a10d'
+        )
         
         x = prng.rc4()
         x.add_entropy(b'12345678')
-        # print x.read(16).encode('hex')
-        assert b16encode(x.read(16)).lower() == b'0a015ada42e721c8ee3d57a0b519a9a8'
+        self.assertEqual(
+            b16encode(x.read(16)).lower(),
+            b'0a015ada42e721c8ee3d57a0b519a9a8'
+        )
         
         x = prng.sober128()
         x.add_entropy(b'12345678')
-        # print x.read(16).encode('hex')
-        assert b16encode(x.read(16)).lower() == b'39e10e5ccaa9a24c26abaf73dbf2e3f6'
+        self.assertEqual(
+            b16encode(x.read(16)).lower(),
+            b'39e10e5ccaa9a24c26abaf73dbf2e3f6'
+        )
         
         x = prng.yarrow()
         x.add_entropy(b'12345678')
-        # print x.read(16).encode('hex')
-        assert b16encode(x.read(16)).lower() == b'f63c36f72ad5098e42ac002243d2cce2'
+        self.assertEqual(
+            b16encode(x.read(16)).lower(),
+            b'f63c36f72ad5098e42ac002243d2cce2'
+        )
 
 
 if __name__ == '__main__':

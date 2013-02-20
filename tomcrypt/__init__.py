25e4730c4614a26cdecd60eb0846e69578353d2c
Mike Boers <mboers@keypics.com>
2013-02-20 14:51:53 -0800

    Linking: Use import to locate _core
    Related to #8

--- tomcrypt/__init__.py
+++ tomcrypt/__init__.py
@@ -1,14 +1,5 @@
 
-import os
 import ctypes
-
-# We need to manually load the _core SO the first time so that we can specify
-# that it use the RTLD_GLOBAL flag. Otherwise (when not on a Mac) the runtime
-# linker will not be able to resolve undefined symbols in the other modules.
-_core_handle = ctypes.CDLL(
-    os.path.join(os.path.dirname(os.path.abspath(__file__)), '_core.so'),
-    ctypes.RTLD_GLOBAL
-)
 
 
 class Error(ValueError):
@@ -19,3 +10,11 @@ class Error(ValueError):
 class LibError(Error):
     pass
 
+
+# We need to manually load the _core the first time so that we can specify
+# that it use the RTLD_GLOBAL flag. Otherwise (when not on a Mac) the runtime
+# linker will not be able to resolve undefined symbols in the other modules.
+# This must also be done after the above exceptions are defined so that they
+# are availible to the core.
+from . import _core
+ctypes.PyDLL(_core.__file__, mode=ctypes.RTLD_GLOBAL)
